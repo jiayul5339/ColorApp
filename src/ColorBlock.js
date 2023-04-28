3264f71da9283596bece699536bdbf0240c4c508
jiayul5339 <98488999+jiayul5339@users.noreply.github.com>
2023-04-28 14:56:40 -0400

    Added the app's basic layout

---
+++ src/ColorBlock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ColorBlock = () => {
+  return (
+    <div className='colorBlock'>
+        <p>Empty Value</p>
+    </div>
+  )
+}
+
+export default ColorBlock
---
+++ src/ColorSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ColorSearch = () => {
+  return (
+    <form >
+        <input className='searchBlock' type='text'/>
+    </form>
+  )
+}
+
+export default ColorSearch
